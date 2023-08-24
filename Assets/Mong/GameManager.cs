302b30f3052ef692d402f3d4d0046204297869f9
wjdahdgus123@gmail.com <wjdahdgus123@gmail.com>
2023-08-24 18:24:43 +0900

    feat : GameManager 추가

---
+++ Assets/Mong/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingleTon<GameManager>
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
