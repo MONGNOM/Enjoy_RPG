d05be8c2308682d9ed95c298bbb398c4ace9aa67
wjdahdgus123@gmail.com <wjdahdgus123@gmail.com>
2023-08-27 23:51:11 +0900

    feat : Magician 추가

---
+++ Assets/Mong/Scripts/Magician.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magician : PlayerInfo
+{
+    protected override void PlayerAttack()
+    {
+        Debug.Log("파이어볼");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
